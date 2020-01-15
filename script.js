67247fa86626910c6a6828c03a0aa562a78ee35b
Jeremiah Rodden <jrodden1@gmail.com>
2020-01-15 12:02:11 -0700

    initial commit with basic script code

---
+++ script.js
@@ -0,0 +1,10 @@
+// Why Timelines are important
+/* 
+
+
+
+*/
+gsap.from("#demo", {duration:1, opacity:0})
+gsap.from("#title", {duration:1, delay:1, opacity:0, scale:0})
+gsap.from("#freds img", {duration:0.5, delay:2, scale:0, stagger:0.5})
+gsap.from("#time", {duration:1, delay:5, opacity:0, xPercent:100})
